cefca01a58d043f59f3295802ae0d0b43bbec73d
AshirAshraf <ashir.ashraf.clt@gmail.com>
2016-11-26 22:17:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+	var express = require('express');
+	var morgan = require('morgan');
+	var path = require('path');
 
-var app = express();
-app.use(morgan('combined'));
+	 var app = express();
+	 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+	app.get('/', function (req, res) {
+	  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+	});
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+	var counter = 0;
+	app.get('/counter', function (req, res){
+		counter = counter + 1;
+	
+		res.send(counter.toString());
+	});
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+	app.get('/ui/style.css', function (req, res) {
+	  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+	});
+
+	app.get('/ui/main.js', function (req, res) {
+		res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+	});
+
+	app.get('/pic.png', function (req, res) {
+	  res.sendFile(path.join(__dirname, 'ui', 'pic.png'));
+	});
+	
+	
+	
+
+
+	var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+	app.listen(8080, function () {
+	  console.log(`ashir online on port ${port}!`);
+	});
